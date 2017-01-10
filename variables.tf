daf703f716c459cb9d8a2e70a73fc489d6f1a816
Hamza Shehu <letters2hamza@gmail.com>
2017-01-10 23:22:45 +0800

    vpc resource files

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+	type = "string"
+	description = "region to launch resources"
+}
+
+variable "aws_profile" {
+	type = "string"
+	description = "profile to launch resources"
+}
