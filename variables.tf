bde6393e23451686b668e439528318390d46d90e
Hamza Shehu <letters2hamza@gmail.com>
2017-01-11 19:31:41 +0800

    add credential variables

--- variables.tf
+++ variables.tf
@@ -1,9 +1,14 @@
+#--------------------------------------------------------------
+# Region and Profile to launch resources
+#--------------------------------------------------------------
 variable "aws_region" {
-	type = "string"
+	type        = "string"
 	description = "region to launch resources"
+	default     = "default_value"
 }
 
 variable "aws_profile" {
-	type = "string"
+	type        = "string"
 	description = "profile to launch resources"
+	default     = "default_value"
 }
