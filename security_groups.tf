e85ed2cf735a17751067e61e5246ec31dcad3a1c
Hamza Shehu <letters2hamza@gmail.com>
2017-01-11 22:50:12 +0800

    add rds security group rules

--- security_groups.tf
+++ security_groups.tf
@@ -29,3 +29,44 @@ resource "aws_security_group" "public" {
 		cidr_blocks = ["0.0.0.0/0"]
 	}
 }
+
+#--------------------------------------------------------------
+# Private SG
+#--------------------------------------------------------------
+resource "aws_security_group" "private" {
+	name 		= "sg_public"
+	description = "used for the private instances"
+	vpc_id 		= "${aws_vpc.ansiform.id}"
+
+	# Access from other SG groups
+	ingress {
+		from_port 	= 0
+		to_port   	= 0
+		protocol  	= "-1"
+		cidr_blocks = ["10.1.0.0/16"]
+	}
+
+	egress {
+		from_port   = 0
+		to_port     = 0
+		protocol    = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
+
+#--------------------------------------------------------------
+# RDS SG
+#--------------------------------------------------------------
+resource "aws_security_group" "rds" {
+	name 		= "sg_public"
+	description = "used for the rds instances"
+	vpc_id 		= "${aws_vpc.ansiform.id}"
+
+	# Access to MYSQL Port
+	ingress {
+		from_port 		= 3306
+		to_port   		= 3306
+		protocol  		= "tcp"
+		security_groups = ["${aws_security_group.public.id}", "${aws_security_group.private.id}"]
+	}
+}
