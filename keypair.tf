fc4a2e3e08f45242853fe25579c7b1c7b83fe9c7
Hamza Shehu <letters2hamza@gmail.com>
2017-01-11 23:11:50 +0800

    add SSH Keypair resource

---
+++ keypair.tf
@@ -0,0 +1,7 @@
+#--------------------------------------------------------------
+# SSH Keypair
+#--------------------------------------------------------------
+resource "aws_key_pair" "auth" {
+	key_name   = "${var.key_name}"
+	public_key = "${file{var.public_key_path}}"
+}
