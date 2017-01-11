9836863933f7af899a1d36a82cd7c5c90e856594
Hamza Shehu <letters2hamza@gmail.com>
2017-01-11 22:15:38 +0800

    add public aws_subnet

--- vpc.tf
+++ vpc.tf
@@ -48,3 +48,13 @@ resource "aws_default_route_table" "private" {
 #--------------------------------------------------------------
 # Subnet - Public
 #--------------------------------------------------------------
+resource "aws_subnet" "public" {
+	vpc_id     				= "${aws_vpc.ansiform.id}"
+	cidr_block 				= "${var.public_cidr}"
+	availability_zone 		= "us-east-1d"
+	map_public_ip_on_launch = true 
+
+	tags{
+		Name = "public"
+	}
+}
