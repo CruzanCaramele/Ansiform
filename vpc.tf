2fd3d96971ee60bbf8336d8cc9c99e3818e3a6cd
Hamza Shehu <letters2hamza@gmail.com>
2017-01-11 22:03:51 +0800

    add internet gateway

---
+++ vpc.tf
@@ -0,0 +1,36 @@
+#--------------------------------------------------------------
+# VPC
+#--------------------------------------------------------------
+resource "aws_vpc" "ansiform" {
+	cidr_block           = "${var.cidr_block}"
+	enable_dns_support   = true
+	enable_dns_hostnames = true
+
+	tags{
+		Name = "ansible"
+	}
+}
+
+#--------------------------------------------------------------
+# Internet Gateway
+#--------------------------------------------------------------
+resource "aws_internet_gateway" "internet_gateway" {
+	vpc_id = "${aws_vpc.ansiform.id}"
+}
+
+#--------------------------------------------------------------
+# Route Table - Public
+#--------------------------------------------------------------
+resource "aws_route_table" "public" {
+	vpc_id = "${aws_vpc.ansiform.id}"
+
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = "${aws_internet_gateway.internet_gateway.id}"
+	}
+
+	tags {
+		Name = "public"
+	}
+}
+
