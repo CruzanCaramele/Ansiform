c24f2aa9650ebbce2fc1f2f35d250a6a6aec7730
Hamza Shehu <letters2hamza@gmail.com>
2017-01-16 21:58:51 +0800

    add dev instance and local-exec provisioner

--- instances.tf
+++ instances.tf
@@ -12,3 +12,33 @@ resource "aws_db_instance" "db" {
   	db_subnet_group_name   = "${aws_db_subnet_group.rds_subnet.name}"
   	vpc_security_group_ids = ["${aws_security_group.rds.id}"]
 }
+
+#--------------------------------------------------------------
+# Dev Instance
+#--------------------------------------------------------------
+resource "aws_instance" "dev" {
+	instance_type = "${var.dev_instance_type}"
+	ami           = "${var.dev_ami}"
+
+	tags {
+		Name = "dev"
+	}
+
+	key_name               = "${aws_key_pair.auth.id}"
+	vpc_security_group_ids = ["${aws_security_group.public.id}"]
+	iam_instance_profile   = "${aws_iam_instance_profile.s3_access.id}"
+	subnet_id              = "${aws_subnet.public.id}"
+
+	provisioner "local-exec" {
+		command = "cat <<EOF > aws_hosts
+[dev]
+${aws_instance.dev.public_ip}
+[dev:vars]
+s3code=${aws_s3_bucket.code.bucket}
+EOF"
+	}
+
+	provisioner "local-exec" {
+		command = "sleep 6m && ansible-playbook -i aws_hosts wordpress.yml"
+	}
+}
