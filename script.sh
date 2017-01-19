7516d9fd072eef3d1b94b1a14117bab3cbe15afe
Hamza Shehu <letters2hamza@gmail.com>
2017-01-19 15:20:51 +0800

    add ntp update to avoid signature issues with AWS time

--- script.sh
+++ script.sh
@@ -1,10 +1,26 @@
-#!/bin/bash
-yum update -y
-yum install -y unzip python git python-devel openssl curl
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-python get-pip.py && pip install awscli
-yum install -y ansible
-wget https://releases.hashicorp.com/terraform/0.8.4/terraform_0.8.4_linux_amd64.zip
+apt-get -y update
+apt-get -y install curl unzip wget
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install awscli
+
 mkdir ~/terraform
+wget https://releases.hashicorp.com/terraform/0.8.4/terraform_0.8.4_linux_amd64.zip
 unzip terraform_0.8.4_linux_amd64.zip -d ~/terraform
-export PATH=$PATH:~/terraform
+# export PATH=$PATH:~/terraform
+
+# install ansible
+apt-add-repository ppa:ansible/ansible
+apt-get -y update
+apt-get -y install ansible
+
+# adjust ntp to avoid AWS Signature issues with time
+apt-get -y install ntp
+service ntp stop
+ntpdate -s us.pool.ntp.org
+service ntp start
+
+# aws route53 create-reusable-delegation-set --caller-reference 12345
+
+vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your shared files: cd /vagrant(B[m"
+echo -e $vagrantTip > /etc/motd
