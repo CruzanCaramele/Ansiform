c35c836245cdb6092b310490ed1b1ad9d1a52ac3
CruzanCaramele <letters2hamza@gmail.com>
2017-02-04 23:50:21 +0800

    add secret key variable

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@
 # AWS Provider Details
 #--------------------------------------------------------------
 provider "aws" {
-	region  = "${var.aws_region}"
-	profile = "${var.aws_profile}"
+	region     = "${var.aws_region}"
+	profile    = "${var.aws_profile}"
+	access_key = "${var.aws_access_key}"
+	secret_key = "${var.aws_secret_key}"
 }
